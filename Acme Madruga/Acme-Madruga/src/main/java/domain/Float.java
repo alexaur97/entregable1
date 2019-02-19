aaa6e0b1eee8512267bdc6f86d58a0d3290bee64
David Morales Llucia <dml94@live.com>
2019-02-19 17:35:23 +0100

    Domain

---
+++ Acme Madruga/Acme-Madruga/src/main/java/domain/Float.java
@@ -0,0 +1,53 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Float extends DomainEntity {
+
+	// Atributos Privados
+
+	private String				title;
+	private String				description;
+	private Collection<String>	photos;
+
+
+	// Getters y Setters
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@NotBlank
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	@ElementCollection
+	public Collection<String> getPhotos() {
+		return this.photos;
+	}
+
+	public void setPhotos(final Collection<String> photos) {
+		this.photos = photos;
+	}
+
+}
