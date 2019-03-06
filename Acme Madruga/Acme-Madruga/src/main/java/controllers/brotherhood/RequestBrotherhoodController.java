145961cf372f63227cce21bb069df44e3b3888db
David Morales Llucia <dml94@live.com>
2019-03-06 20:41:08 +0100

    Reject Requests

--- Acme Madruga/Acme-Madruga/src/main/java/controllers/brotherhood/RequestBrotherhoodController.java
+++ Acme Madruga/Acme-Madruga/src/main/java/controllers/brotherhood/RequestBrotherhoodController.java
@@ -5,7 +5,10 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.AdministratorService;
@@ -13,6 +16,7 @@ import services.ProcessionService;
 import services.RequestService;
 import controllers.AbstractController;
 import domain.Request;
+import forms.RejectForm;
 
 @Controller
 @RequestMapping("request/brotherhood")
@@ -48,6 +52,34 @@ public class RequestBrotherhoodController extends AbstractController {
 		result.addObject("resquestURI", "/request/brotherhood/list.do");
 
 		return result;
+	}
+
+	@RequestMapping(value = "/reject", method = RequestMethod.GET)
+	public ModelAndView reject(@RequestParam final int requestId) {
+		final ModelAndView res = new ModelAndView("request/reject");
+		RejectForm rejectForm;
+		rejectForm = new RejectForm();
+		try {
+			rejectForm.setRequestId(requestId);
+			res.addObject("rejectForm", rejectForm);
+		} catch (final Throwable oops) {
+			res.addObject("message", "request.commit.error");
+		}
+		return res;
+	}
+
+	@RequestMapping(value = "/reject", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(final RejectForm rejectForm, final BindingResult binding) {
+		ModelAndView res = new ModelAndView("redirect:/request/brotherhood/list.do");
+		try {
+			this.requestService.reject(rejectForm.getRequestId(), rejectForm.getExplanation());
+			res = new ModelAndView("redirect:/request/brotherhood/list.do");
+		} catch (final Throwable oops) {
+			res.addObject("message", "request.commit.error");
+
+		}
+
+		return res;
 	}
 
 }
